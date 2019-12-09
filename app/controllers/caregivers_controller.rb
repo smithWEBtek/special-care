3b3ce1d58b922fac5a9569f08061827108ae1dc1
smithwebtek <brad@smithwebtek.com>
2019-12-08 19:24:47 -0500

    Add controller for caregiver and document

--- app/controllers/caregivers_controller.rb
+++ app/controllers/caregivers_controller.rb
@@ -1,2 +1,65 @@
 class CaregiversController < ApplicationController
+  before_action :set_caregiver, only: [:show, :edit, :update, :destroy]
+
+  def index
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @caregivers }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @caregiver }
+    end
+  end
+
+  def new
+    @caregiver = Caregiver.new
+  end
+
+  def create
+    @caregiver = Caregiver.find_or_create_by(name: caregiver_params[:name])
+    if @caregiver.save
+      respond_to do |format|
+        format.html { redirect_to caregiver_path(@caregiver), layout: false }
+        format.json { render json: @caregiver }
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    @caregiver.update(caregiver_params)
+    if @caregiver.save
+      flash[:notice] = "Caregiver updated."
+      redirect_to caregiver_path(@caregiver)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @caregiver.reset_user_caregiver_id
+    if @caregiver.delete
+      flash[:notice] = "Caregiver deleted, user must choose new caregiver."
+      redirect_to caregivers_path
+    else
+      flash[:notice] = @caregiver.errors.full_messages
+      redirect_to caregiver_path(@caregiver)
+    end
+  end
+
+  private
+    def set_caregiver
+      @caregiver = Caregiver.find_by_id(params[:id])
+    end
+    def caregiver_params
+      params.require(:caregiver).permit(:name)
+    end
 end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,66 @@
 class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update, :destroy]
+
+  def index
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @documents }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @document }
+    end
+  end
+
+  def new
+    @document = Document.new
+  end
+
+  def create
+    @document = Document.find_or_create_by(name: document_params[:name])
+    if @document.save
+      respond_to do |format|
+        format.html { redirect_to document_path(@document), layout: false }
+        format.json { render json: @document }
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    @document.update(document_params)
+    if @document.save
+      flash[:notice] = "Caregiver updated."
+      redirect_to document_path(@document)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @document.reset_user_document_id
+    if @document.delete
+      flash[:notice] = "Caregiver deleted, user must choose new document."
+      redirect_to documents_path
+    else
+      flash[:notice] = @document.errors.full_messages
+      redirect_to document_path(@document)
+    end
+  end
+
+  private
+    def set_document
+      @document = Document.find_by_id(params[:id])
+    end
+    def document_params
+      params.require(:document).permit(:name)
+    end
 end
+
